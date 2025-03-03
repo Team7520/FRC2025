e7c8ed1afced6694622b5b02eac16d2fdd5c4ea5
Matthew Liang <woodyl_2009@hotmail.com>
2025-03-03 08:25:57 -0500

    Updated LightingSubsystem

--- src/main/java/frc/robot/subsystems/LightingSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightingSubsystem.java
@@ -1,47 +1,75 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems;// Define the package
 
-import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle;// Import the CANdle libraries and the LED animation libraries
 import com.ctre.phoenix.led.RainbowAnimation;
 import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.FireAnimation;
 
-public class LightingSubsystem {
-    private final static LightingSubsystem INSTANCE = new LightingSubsystem();
-    public final CANdle candle = new CANdle(17); // creates a new CANdle with ID 17
-    
-    public static LightingSubsystem getInstance() {
-      return INSTANCE;
-    }
-    
-    private LightingSubsystem() {
-      candle.setLEDs(255, 255, 255); // set the CANdle LEDs to white      
-    }
+/* 
+* Class that controls robot lighting through the CANdle (excluding the RSL)
+* Note: Until CTRE implements CANdle support in Phoenix 6, Phoenix 5 is required.
+*/
+// Flashing white, fire animation
+public class LightingSubsystem {// Define a class
+private final static LightingSubsystem INSTANCE = new LightingSubsystem();// Set INSTANCE to a new instance of LightingSubsystem
+public final CANdle candle = new CANdle(17); // creates a new CANdle with ID 17
+public final int numLED = 32;// Change to adjust number of LEDs
+public static LightingSubsystem getInstance() {// Return the instance when getInstance is called in the robot code
+  return INSTANCE;
+}
 
-    public void setLEDs(int r, int g, int b) {
-      candle.setLEDs(r, g, b);
-    }
+public void ColourFlowAnimate(int r, int g, int b) {// Colour flow animation
+  ColorFlowAnimation colourFlowAnim = new ColorFlowAnimation(r, g, b);
+  candle.animate(colourFlowAnim);
+}
 
-    public void RainbowAnimate() {
-      // create a rainbow animation:
+private LightingSubsystem() {// Startup code
+  candle.clearAnimation(0);// Clear animations
+  candle.clearAnimation(1);
+  ColourFlowAnimate(0, 0, 255);// Colour flow animate our team colour
+}
 
-      // - max brightness
+public void setLEDs(int r, int g, int b) {// Function to change LED colours
+  candle.setLEDs(r, g, b);
+}
 
-      // - half speed
+public void RainbowAnimate() {// Rainbow animation
+  // create a rainbow animation:
 
-      // - 32 LEDs
+  // - max brightness
 
-      RainbowAnimation rainbowAnim = new RainbowAnimation(1, 0.5, 32);
+  // - half speed
 
-      candle.animate(rainbowAnim);
-    }
+  // - numLED LEDs
 
-    public void StrobeAnimate(int r, int g, int b) {
-      StrobeAnimation strobeAnim = new StrobeAnimation(r, g, b);
+  RainbowAnimation rainbowAnim = new RainbowAnimation(1, 0.5, numLED);
 
-      candle.animate(strobeAnim);
-    }
+  candle.animate(rainbowAnim, 0);
+}
 
-    public void AnimateTeam() {
-      // Set LEDs to strobe team colour
-      StrobeAnimate(255, 0, 0);
-    }
+public void StrobeAnimate(int r, int g, int b, int w) {// Strobe animation
+  StrobeAnimation strobeAnim = new StrobeAnimation(r, g, b, w, 0.1, numLED);
+
+  candle.animate(strobeAnim, 0);
+}
+
+public void AnimateTeam() {
+  // Set LEDs to team colour
+  StrobeAnimate(255, 0, 0, 0);
+}
+
+public void FlashingWhite() {// Flash all LEDs white
+  StrobeAnimate(191, 191, 191, 0);
+}
+
+public void FireAnimate() {// Fire animation
+  FireAnimation fireAnimL = new FireAnimation(1, 0.1, numLED, 1, 1, false, 0);// Left side animation
+  FireAnimation fireAnimR = new FireAnimation(1, 0.1, numLED, 1, 1, true, 20);// Right side animation (needs offset and reverse)
+  //TwinkleAnimation fireAnimL = new TwinkleAnimation(242, 145, 12, 0, 1, numLED, null);
+  //TwinkleAnimation fireAnimR = new TwinkleAnimation(242, 145, 12, 0, 1, numLED, null);
+  candle.animate(fireAnimL, 0);
+  candle.animate(fireAnimR, 1);
+}
+
 }
